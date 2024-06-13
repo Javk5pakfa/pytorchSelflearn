8006925c3db12313c138f03d2a4e459363bc201b
Jack Hu <zxh496@miami.edu>
2024-06-13 14:26:21 -0400

    Update data generator to make consistent data with deviations and missing values

--- src/synthetic_data_gen.py
+++ src/synthetic_data_gen.py
@@ -9,12 +9,13 @@ def plot_2plots(x, y1, y2):
     Plot two plots on same figure.
     :param x: x range.
     :param y1: actual y data.
-    :param y2: actual x data.
+    :param y2: generated y data.
     :return: Nothing.
     """
 
-    plt.scatter(x, y1, marker='o', color='blue')
+    plt.plot(x, y1, color='blue')
     plt.scatter(x, y2, marker='*', color='red')
+    plt.legend(['Actual', 'Generated'])
     plt.show()
 
 
@@ -30,15 +31,43 @@ def data_out(data: pd.DataFrame, filename: str = 'synthetic_data.csv'):
     data.to_csv(default_path + "/" + filename, index=False)
 
 
-if __name__ == "__main__":
-    x_range = np.linspace(0, 1, 100)
-    y_actual = np.exp(x_range)
-    y_synthetic = y_actual + np.random.uniform(-0.1, 0.1, 100)
-    # print(y_actual[:10] - y_synthetic[:10])
+def data_generator(x_range: np.linspace,
+                   y_actual,
+                   random_weight=np.random.uniform(-0.1, 0.1, 100)):
+    """
+    Dataset generator for synthetic data.
+    :param x_range: range of x values.
+    :param y_actual: values of y actual data.
+    :param random_weight: random weight for random data.
+    :return: pandas Dataframe object.
+    """
 
-    # Plot the generated data.
-    # plot_2plots(x_range, y_actual, y_synthetic)
+    y_out = y_actual
+
+    # Filter out non-zero values.
+    y_out[y_out <= 0.0] = np.nan
+
+    # Generate a random array with the same shape as y_func_2
+    random_array = np.random.rand(*y_out.shape)
+
+    # Create a mask with a probability between 0 and 1.
+    mask = random_array < 0.55
+
+    # Set elements to np.nan based on the mask
+    y_out[mask] = np.nan
+
+    return pd.DataFrame({'x': x_range,
+                         'y': y_out + random_weight})
+
+
+if __name__ == "__main__":
 
     # Output data.
-    df = pd.DataFrame({'x': x_range, 'y': y_synthetic})
-    data_out(df)
+    num_data = 100
+    x_data = np.linspace(0, 2, num_data)
+
+    a, b, c, d, f, g = -6.5, -2, 5, 1.5, 1, 0.3
+    y_func_2 = g + a * (x_data - f)**4 + b * (x_data - f)**3 + c * (x_data - f)**2 + d * (x_data - f)
+
+    ds = data_generator(x_data.copy(), y_func_2.copy(), np.linspace(-0.2, 0.2, num_data))
+    plot_2plots(x_data, y_func_2, ds.y)
