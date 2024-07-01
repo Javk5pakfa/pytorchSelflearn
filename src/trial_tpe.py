4537c94ae8116f3a04c90aa8384b0ce73d78084c
Jack Hu <zxh496@miami.edu>
2024-07-01 12:25:21 -0400

    Rework PE such that only non-empty values are parsed thru

--- src/trial_tpe.py
+++ src/trial_tpe.py
@@ -34,23 +34,30 @@ class TrialTPE(TPE):
         self.__data_lin = nn.Linear(1, self.__d_emb)
 
     def forward(self, obs: Observation):
-        # No need to split the bands. Just embed the y values and x values
-        # to the same dimension.
 
         # Load dataset.
         obs_data = obs.get_data()
         n_pos = len(obs_data.x)
         x_data = obs_data.x.to_numpy()
-        temp_y = obs_data.y.to_numpy()
-        y_data = pad_nan(temp_y)  # Nan data is padded here.
+        y_data = obs_data.y.to_numpy()
 
-        # Put y values thru a linear layer.
+        # Positional encoding on the x-axis.
+        x_tensor_temp = torch.Tensor(self.__positional_encoding(n_pos))
+        x_tensor_final = torch.empty(0, self.__d_emb)
+
+        # Put y values through a linear layer. Since we only want to pass
+        # non-nan values to the next layer, the positions of those in x-axis
+        # will also be included, with the rest discarded.
         y_emb_vals = []
-        for val in y_data:
-            y_emb_vals.append(self.__data_lin(torch.Tensor([val])))
-        y_tensor = torch.stack(y_emb_vals)
-        x_tensor = torch.Tensor(self.__positional_encoding(n_pos))
-        self.__xy_rep_tensor = x_tensor + y_tensor
+        for index, val in enumerate(y_data):
+            if not np.isnan(val):
+                x_tensor_final = torch.cat((x_tensor_final, torch.reshape(x_tensor_temp[index], (1, self.__d_emb))), dim=0)
+                y_emb_vals.append(self.__data_lin(torch.Tensor([val])))
+        y_tensor_final = torch.stack(y_emb_vals)
+
+        # Finally, linearly combine encodings.
+        assert x_tensor_final.shape == y_tensor_final.shape
+        self.__xy_rep_tensor = x_tensor_final + y_tensor_final
 
     def __get_angles(self, pos, i):
         angle_rates = 1 / np.power(10000,
@@ -113,4 +120,8 @@ if __name__ == '__main__':
         encoder.forward(observe)
         encoded_data.append(encoder.get_representation())
 
-    print(encoded_data)
+    for index, val in enumerate(encoded_data):
+        if index < 10:
+            print("Index: {}, Tensor: {}".format(index, val))
+        else:
+            break
